16f2a3b41be5cbd9a64a2e522205c10749f729ff
maxkl <klein@spsejecna.cz>
2025-01-30 09:48:16 +0100

    commit

---
+++ src/Animal.java
@@ -0,0 +1,17 @@
+public abstract class Animal {
+
+    protected String type;
+    protected String name;
+    protected Size size;
+    protected String specialCare;
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                ", size=" + size +
+                ", specialCare='" + specialCare + '\'' +
+                '}';
+    }
+}
---
+++ src/Flower.java
@@ -0,0 +1,18 @@
+public abstract class Flower {
+
+    protected String name;
+    protected double price;
+    protected double neededArea;
+    protected double chanceOfGrowth;
+
+    @Override
+    public String toString() {
+        return "Flower{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", neededArea=" + neededArea +
+                ", chanceOfGrowth=" + chanceOfGrowth +
+                '}';
+    }
+
+}
